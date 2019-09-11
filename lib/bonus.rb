a3849fd3e1e98606ecc6bac00f8048159730cbf0
vincent servio <vincentservio@gmail.com>
2019-09-11 04:27:27 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,3 +1,4 @@
+
 def bonus
   epic_tragedy = {
    :montague => {
@@ -21,6 +22,9 @@ def bonus
   }
 
   #code your solution here:
+epic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy[:capulet][:heroine][:status] = "dead"
+puts epic_tragedy
 
   
 
