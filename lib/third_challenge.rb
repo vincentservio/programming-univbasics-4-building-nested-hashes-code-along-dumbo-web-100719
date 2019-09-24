df9c70ecb8b9393c1cecad4ad17071ac1d727155
vincent servio <vincentservio@gmail.com>
2019-09-24 14:43:43 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,6 +3,7 @@ def third_challenge
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
+<<<<<<< HEAD
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"},
       {name: "Mercutio", age: "18", attitude: "hoy-headed"}
@@ -18,6 +19,29 @@ def third_challenge
        {name: "Nurse",
        age: "44",
        attitude: "worried"}
+=======
+      :hero => {name: "Romeo", age: "15"},
+      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"}
+   
+      
+      }]
+   }, 
+   :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name:"Lady capulet", age: "51"},
+      :heroine => {name: "Juliet",
+        age: "15",
+        status: "alive"},
+      :heroine_friends => [ { 
+         name: "steven",
+        age: "30",
+        attitude: "confused"
+       },
+       {name: "Nurse",
+       age: "44",
+       attitude: "worried"
+      }
+>>>>>>> 79f999596547908f358e7e605bfc2c055a48d41f
       ]
    }
 }
